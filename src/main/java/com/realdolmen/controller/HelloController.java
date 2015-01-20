c05061a7ca8696328ecab813a4ee5726f23952c2
Kevin De Man <kevin.deman@realdolmen.com>
2015-01-20 10:28:26 +0100

    Basic config with web.xml

---
+++ src/main/java/com/realdolmen/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.realdolmen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping(value = "/greeting")
+	public String sayHello(Model model) {
+		model.addAttribute("greeting", "Hello, world!");
+		// no view resolver yet!
+		return "hello.jsp";
+	}
+	
+}
